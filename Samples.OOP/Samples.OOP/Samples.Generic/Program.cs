e27f5832d4caff342a1ccd2099105e78e5bcb155
Luis Alberto Pintado Goicochea <luiscol50@hotmail.com>
2018-05-29 21:00:20 -0500

    added new samples of generics

--- Samples.OOP/Samples.OOP/Samples.Generic/Program.cs
+++ Samples.OOP/Samples.OOP/Samples.Generic/Program.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using Samples.Generics;
+using System;
 
 namespace Samples.Generic
 {
     class Program
     {
+        /// <summary>
+        /// https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/generics/
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ScenarioTwo();
+        }
+
+        /// <summary>
+        /// Create a generic method
+        /// </summary>
+        public static void ScenarioOne()
+        {
+            var a = 10;
+            var b = 20;
+            Console.WriteLine("Value of a should be A: " + a);
+            Console.WriteLine("Value of b should be B:  " + b);
+            SampleGeneric.Swap<int>(ref a, ref b);
+            System.Console.WriteLine("----------");
+            Console.WriteLine("Value of a should be A: " + a);
+            Console.WriteLine("Value of b should be B:  " + b);
+
+            // Keep the console open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Constraints on type parameters
+        /// </summary>
+        public static void ScenarioTwo()
+        {
+            // GenericList<Programer> list = new GenericList<Programer>(); // it's not valid
+            GenericList<Dancer> list = new GenericList<Dancer>();
+            list.AddHead(new Dancer("miguel", 1));
+            list.AddHead(new Dancer("carlos", 2));
+            list.AddHead(new Dancer("kakaroto", 3));
+
+            Console.WriteLine(list.FindFirstOccurrence("kakaroto").ID);
+
+            // Keep the console open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
+
 }
